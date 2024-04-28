dcf6a863a1eb172b0da84ccd7d62ef4dd50d3deb
zeredogo <zeredogo@gmail.com>
2024-04-28 18:47:06 +0100

    update commit

--- Backend/Node.sol
+++ Backend/Node.sol
@@ -6,5 +6,5 @@ contract Node{
     uint256 public Price;
     address public Sender; 
 
-    function getName(string memory _name) public view re 
+    function getName(string memory _name) public view ret 
 }
