1ac7941dc81156f9e8fa5e362c4d1ca606dfafe1
zeredogo <zeredogo@gmail.com>
2024-05-01 20:21:47 +0100

    update commit

--- Backend/Node.sol
+++ Backend/Node.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.8.3;
 
 contract Node{
     string public Name;
-    uint256 public Price;
+    uint256 public  Price;
     address public Sender; 
 
     function getName(string memory _name) public view returns{
         return _name = 'salcf';
     } 
 
-    function getPrice(uint256 _price) public view returns{
-        return _price = ;
+    function getPrice(uint256 _price) public payable returns{
+        return _price = msg.value;
     }
 
     function getSender(address _sender) public view returns{
-        return _sender = msg.sender
+        return _sender = msg.sender;
     } 
 }
