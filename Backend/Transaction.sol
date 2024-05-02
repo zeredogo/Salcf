affd8ee45bfdc8590bb6f053a00664a833013d68
zeredogo <zeredogo@gmail.com>
2024-05-02 22:36:32 +0100

    update commit

---
+++ Backend/Transaction.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Indentifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transaction {
+    uint256 transactionCounter;
+
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+    
+}
