2ea234105545b26b4624813715afaa66d5556204
zeredogo <zeredogo@gmail.com>
2024-05-04 22:41:58 +0100

    update commit

--- Backend/Transaction.sol
+++ Backend/Transaction.sol
@@ -7,6 +7,6 @@ contract Transaction {
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
     struct TransferStruct{
-        ad
+        add
     }
 }
